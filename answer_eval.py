db306857b80bb12d964cb2efddbfa890178822e6
Adnantium <adnantium@gmail.com>
2024-01-31 16:40:15 -0500

    3

--- answer_eval.py
+++ answer_eval.py
@@ -1,4 +1,3 @@
-
 from rich import print
 from langchain_core.pydantic_v1 import BaseModel, Field
 from langchain_core.output_parsers import JsonOutputParser
@@ -10,14 +9,18 @@ from typing import List
 import json
 
 import langchain
+
 langchain.debug = True
 
 # 1. Completeness: Does the answer cover all the important informaton expected in an excellent answer? What information is missing?
 # 2. Correctness: Is answer free of errors and inaccurate information?
 # 3. Grammar: Does it use proper english grammar and spelling?
 
-
-DEFAULT_EVAL_CRITERIA = ["Completeness", "Correctness", "Grammar",]
+DEFAULT_EVAL_CRITERIA = [
+    "Completeness",
+    "Correctness",
+    "Grammar",
+]
 # DEFAULT_EVAL_CRITERIA = {
 #     "Completeness": "Does the answer cover all the important informaton expected in an excellent answer? What information is missing?",
 #     "Correctness": "Is answer free of errors and inaccurate information?",
@@ -34,6 +37,8 @@ ANSWER_EVAL_PROMPT_TEMPLATE = """
     The "comments" should be concise, addressed directly to the author, and should exclusively comprise constructive criticisms and detailed, specific recommendations for enhancements.
     
     {format_instructions}
+    
+    The "evaluator_name" field value should be: {evaluator_name}
 
     **Question**: "{question}"
 
@@ -44,28 +49,24 @@ ANSWER_EVAL_PROMPT_TEMPLATE = """
 class CriteriaScore(BaseModel):
     name: str = Field(description="The name of the criteria e.g Grammar")
     definition: str = Field(description="The definition used for the criteria")
-    # evaluator: str = Field(description="Name of the system that did the evaluation")
     comments: str = Field(
         description="Helpful comments on any problems with the answer and potentional improvments")
-    score: int = Field(
-        description="The scrore given to the answer for the given criteria")
+    score: int = Field(description="The score given to the answer for the given criteria")
 
 
 class Evaluation(BaseModel):
-    question: str = Field(description="The question that was asked")
-    answer: str = Field(description="The answer given for the question")
-    scores: List[CriteriaScore] = Field(
-        description="List of scores for each evaluation criteria")
+    evaluator_name: str = Field(description="Name of the system that did the evaluation")
+    scores: List[CriteriaScore] = Field(description="List of scores for each evaluation criteria")
+
+
+# class QAEvaluation(BaseModel):
+#     question: str = Field(description="The question that was asked")
+#     answer: str = Field(description="The answer given for the question")
+#     evaluations: List[Evaluation] = Field(
+#         description="Evaluation scores from multiple systems")
+
 
 def get_criteria_list_text(criterion=None):
-    """Takes dict (with specific requirements) or list/tuple
-
-    Args:
-        criterion (_type_, optional): _description_. Defaults to None.
-
-    Returns:
-        _type_: _description_
-    """
     criterion = criterion or DEFAULT_EVAL_CRITERIA
     criteria_list = ""
     if type(criterion) is dict:
@@ -78,32 +79,66 @@ def get_criteria_list_text(criterion=None):
     return criteria_list
 
 
-def get_answer_eval_chain():
+AVAILABLE_MODELS = [
+    'gpt-3.5-turbo',
+    'gpt-3.5-turbo-1106',
+    'gpt-4-1106-preview',
+    'gpt-4-turbo-preview',
+]
+
+
+def get_model(model_name=None):
+    model_name = model_name or AVAILABLE_MODELS[0]
+    if model_name not in AVAILABLE_MODELS:
+        raise ValueError(f"Model name must be one of {AVAILABLE_MODELS}")
+    model = ChatOpenAI(model_name=model_name, temperature=0)
+    return model
+
+
+def get_answer_single_eval_chain(model_name=None):
     template = ANSWER_EVAL_PROMPT_TEMPLATE
     pydantic_parser = PydanticOutputParser(pydantic_object=Evaluation)
+    model = get_model(model_name)
     prompt = PromptTemplate(
         template=template,
         input_variables=["question", "answer"],
         partial_variables={
             "format_instructions": pydantic_parser.get_format_instructions(),
-            "criteria_list_text": get_criteria_list_text()
+            "criteria_list_text": get_criteria_list_text(),
+            "evaluator_name": model.model_name
         },
     )
-    model = ChatOpenAI(model="gpt-3.5-turbo")
     chain = prompt | model | pydantic_parser
     return chain
 
 
-# answer_eval_chain = get_answer_eval_chain()
-# inputs = {
-#     'question': 'What are the differences and similarities of plant and animal cells?',
-#     'answer': """Both plant and animal cells have many organelles in common.
-#         They both include organelles such as ribosomes, mitochondrion, nuclei, and cell membranes.
-#         Plant cells also contain a cell wall, which animal cells do not contain.""",
-#     'criteria': '',
-# }
+def get_answer_multi_eval(question: str, answer: str):
+    chains = [get_answer_single_eval_chain(model_name) for model_name in AVAILABLE_MODELS]
+    inputs = {
+        'question': question,
+        'answer': answer,
+    }
+    all_evaluations = [chain.invoke(inputs) for chain in chains]
+    # all_evaluations = []
+    # for chain in chains:
+    #     response = chain.invoke(inputs)
+    #     all_evaluations.append(response)
+    # print(all_evaluations)
+    return all_evaluations
+
+
+question = 'What are the differences and similarities of plant and animal cells?'
+answer = """Both plant and animal cells have many organelles in common.
+    They both include organelles such as ribosomes, mitochondrion, nuclei, and cell membranes.
+    Plant cells also contain a cell wall, which animal cells do not contain."""
+
+# answer_eval_chain = get_answer_single_eval_chain()
+# inputs = { 'question': question, 'answer': answer, }
 # response: Evaluation = answer_eval_chain.invoke(inputs)
 # # response_js = response.json()
 # # print(json.dumps(response_js, indent=2))
 # print(response)
+# pass
+
+# all_evals = get_answer_multi_eval(question, answer)
 # pass
--- answer_eval_server.py
+++ answer_eval_server.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
-from answer_eval import get_answer_eval_chain, Evaluation
+from typing import List
+from langserve import add_routes
 from fastapi import FastAPI
 from langchain_community.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from langchain_core.runnables.base import RunnableSequence
 
+from answer_eval import get_answer_single_eval_chain, Evaluation
+
 import langchain
+
 langchain.debug = True
-
-from langserve import add_routes
-
-
 
 app = FastAPI(
     title="AnswerEvallm",
@@ -20,22 +20,11 @@ app = FastAPI(
 
 add_routes(
     app,
-    get_answer_eval_chain(),
+    get_answer_single_eval_chain(),
     path="/answer_eval",
     output_type=Evaluation,
 )
 
-# joke_model = ChatOpenAI()
-# prompt = ChatPromptTemplate.from_template("tell me a joke about {topic}")
-# joke_chain = prompt | joke_model
-# add_routes(
-#     app,
-#     joke_chain,
-#     path="/joke",
-# )
-
-
 if __name__ == "__main__":
     import uvicorn
-
     uvicorn.run(app, host="localhost", port=8000)
